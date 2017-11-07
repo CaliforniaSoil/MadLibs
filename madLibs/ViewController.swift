91bf7524889a87131c711afe8dd8ef6c3254b891
Ricardo Rojas <xrojas@att.net>
2017-11-07 12:56:06 -0800

    Initial Commit

---
+++ madLibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  madLibs
+//
+//  Created by Ricardo Rojas on 11/7/17.
+//  Copyright © 2017 Ricardo Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
